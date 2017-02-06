cb93ffbc38fed997ed041d22c9b2f9889aa15d0b
Crispin Read <crispin.read@gmail.com>
2017-02-06 13:27:11 +0000

    add new API key to newsletter sub service

--- php/newsletter-subscribe.php
+++ php/newsletter-subscribe.php
@@ -10,7 +10,7 @@ include('./mailchimp/mailchimp.php');
 use \DrewM\MailChimp\MailChimp;
 
 // Step 1 - Set the apiKey - How get your Mailchimp API KEY - http://kb.mailchimp.com/article/where-can-i-find-my-api-key
-$apiKey 	= '3d5bf0663157fd156afc51e83be0d5a5-us4';
+$apiKey 	= '127632840971a30d7bbbb870d51e95fa-us4';
 
 // Step 2 - Set the listId - How to get your Mailchimp LIST ID - http://kb.mailchimp.com/article/how-can-i-find-my-list-id
 $listId 	= '9f4a28d829';
